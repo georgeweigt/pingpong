8d9e086c835a8cb6ddaa0f64f7f40966df8aa337
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-01-02 16:06:25 -0700

    Update ec.c

--- src/ec.c
+++ src/ec.c
@@ -2160,35 +2160,35 @@ uint32_t *p256, *q256, *gx256, *gy256, *a256, *b256;
 
 // secp256k1
 
-#define P  "FFFFFFFF" "FFFFFFFF" "FFFFFFFF" "FFFFFFFF" "FFFFFFFF" "FFFFFFFF" "FFFFFFFE" "FFFFFC2F"
-#define Q  "FFFFFFFF" "FFFFFFFF" "FFFFFFFF" "FFFFFFFE" "BAAEDCE6" "AF48A03B" "BFD25E8C" "D0364141"
-#define GX "79BE667E" "F9DCBBAC" "55A06295" "CE870B07" "029BFCDB" "2DCE28D9" "59F2815B" "16F81798"
-#define GY "483ADA77" "26A3C465" "5DA4FBFC" "0E1108A8" "FD17B448" "A6855419" "9C47D08F" "FB10D4B8"
-#define A  "0"
-#define B  "7"
+#define STR_P  "FFFFFFFF" "FFFFFFFF" "FFFFFFFF" "FFFFFFFF" "FFFFFFFF" "FFFFFFFF" "FFFFFFFE" "FFFFFC2F"
+#define STR_Q  "FFFFFFFF" "FFFFFFFF" "FFFFFFFF" "FFFFFFFE" "BAAEDCE6" "AF48A03B" "BFD25E8C" "D0364141"
+#define STR_GX "79BE667E" "F9DCBBAC" "55A06295" "CE870B07" "029BFCDB" "2DCE28D9" "59F2815B" "16F81798"
+#define STR_GY "483ADA77" "26A3C465" "5DA4FBFC" "0E1108A8" "FD17B448" "A6855419" "9C47D08F" "FB10D4B8"
+#define STR_A  "0"
+#define STR_B  "7"
 
 #else
 
 // secp256r1
 
-#define P  "ffffffff00000001000000000000000000000000ffffffffffffffffffffffff"
-#define Q  "ffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551"
-#define GX "6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296"
-#define GY "4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5"
-#define A  "FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC"
-#define B  "5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B"
+#define STR_P  "ffffffff00000001000000000000000000000000ffffffffffffffffffffffff"
+#define STR_Q  "ffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551"
+#define STR_GX "6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296"
+#define STR_GY "4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5"
+#define STR_A  "FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC"
+#define STR_B  "5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B"
 
 #endif
 
 void
 ec_init(void)
 {
-	p256 = ec_hexstr_to_bignum(P);
-	q256 = ec_hexstr_to_bignum(Q);
-	gx256 = ec_hexstr_to_bignum(GX);
-	gy256 = ec_hexstr_to_bignum(GY);
-	a256 = ec_hexstr_to_bignum(A);
-	b256 = ec_hexstr_to_bignum(B);
+	p256 = ec_hexstr_to_bignum(STR_P);
+	q256 = ec_hexstr_to_bignum(STR_Q);
+	gx256 = ec_hexstr_to_bignum(STR_GX);
+	gy256 = ec_hexstr_to_bignum(STR_GY);
+	a256 = ec_hexstr_to_bignum(STR_A);
+	b256 = ec_hexstr_to_bignum(STR_B);
 
 	ec_malloc_count = 0;
 }
