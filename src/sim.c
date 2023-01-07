6010388e00b3cf2720fad0fdac3925a4ee284941
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-01-06 23:17:09 -0700

    Update sim.c

--- src/sim.c
+++ src/sim.c
@@ -1,16 +1,21 @@
 // node simulator
 
-struct node initiator, recipient;
+struct node initiator; // Alice
+struct node recipient; // Bob
 
 void
 sim(void)
 {
 	int listen_fd;
 
+	// generate keys
+
 	ec_genkey(initiator.private_key, initiator.public_key);
 	ec_genkey(recipient.private_key, recipient.public_key);
 
 	memcpy(initiator.peer_public_key, recipient.public_key, 64); // Alice knows Bob's public key
+
+	// establish connection
 
 	listen_fd = start_listening(30303);
 	initiator.fd = client_connect("127.0.0.1", 30303);
@@ -20,7 +25,7 @@ sim(void)
 
 	printf("fd %d %d\n", initiator.fd, recipient.fd);
 
-	send_auth_msg(&initiator); // send from initiator to recipient
+	send_auth_msg(&initiator); // send from Alice to Bob
 
 	close(initiator.fd);
 	close(recipient.fd);
