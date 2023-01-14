f336feee99a5b0549188a51f78972fec92217df0
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-01-14 10:56:51 -0700

    Update secrets.c

--- src/secrets.c
+++ src/secrets.c
@@ -1,13 +1,13 @@
 void
 secrets(struct node *p)
 {
-	uint8_t ephemeral_key[32];
+	uint8_t ephemeral_secret[32];
 	uint8_t shared_secret[32];
 	uint8_t buf[64];
 
-	// ephemeral_key = auth_private_key * ack_public_key
+	// ephemeral_secret = auth_private_key * ack_public_key
 
-	ec_ecdh(ephemeral_key, p->auth_private_key, p->ack_public_key);
+	ec_ecdh(ephemeral_secret, p->auth_private_key, p->ack_public_key);
 
 	// shared_secret = keccak256(ephemeral_key || keccak256(ack_nonce || auth_nonce))
 
@@ -16,20 +16,20 @@ secrets(struct node *p)
 
 	keccak256(buf + 32, buf, 64);
 
-	memcpy(buf, ephemeral_key, 32);
+	memcpy(buf, ephemeral_secret, 32);
 
 	keccak256(shared_secret, buf, 64);
 
-	// aes_secret = keccak256(ephemeral_key || shared_secret)
+	// aes_secret = keccak256(ephemeral_secret || shared_secret)
 
-	memcpy(buf, ephemeral_key, 32);
+	memcpy(buf, ephemeral_secret, 32);
 	memcpy(buf + 32, shared_secret, 32);
 
 	keccak256(p->aes_secret, buf, 64);
 
-	// mac_secret = keccak256(ephemeral_key || aes_secret)
+	// mac_secret = keccak256(ephemeral_secret || aes_secret)
 
-	memcpy(buf, ephemeral_key, 32);
+	memcpy(buf, ephemeral_secret, 32);
 	memcpy(buf + 32, p->aes_secret, 32);
 
 	keccak256(p->mac_secret, buf, 64);
