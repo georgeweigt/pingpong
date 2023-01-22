3ca875ae49a5f214862502bacf89248fa120b3ea
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-01-22 11:18:18 -0700

    Update send_hello.c

--- src/send_hello.c
+++ src/send_hello.c
@@ -4,23 +4,38 @@ send_hello(struct node *p)
 	int err;
 	struct atom *msgid, *msgdata;
 
+	// msg id
+
 	push_string(NULL, 0); // empty string ""
 	msgid = pop();
 
 	// msg data
 
-	push_number(5); // protocol version 5
+	// protocol version 5
 
-	push_string((uint8_t *) "pingpong", 3); // client software
+	push_number(5);
 
-	push_string((uint8_t *) "eth", 3); // capability
+	// client software
+
+	push_string((uint8_t *) "pingpong", 3);
+
+	// capability (eth/67)
+#if 1
+	push_string((uint8_t *) "eth", 3);
 	push_number(67);
 	list(2);
 	list(1); // [ ["eth",67] ]
+#else
+	list(0); // empty list [], no capability, geth prints 'useless peer'
+#endif
 
-	push_string(NULL, 0); // listen port
+	// listen port (none)
 
-	push_string(p->public_key, 64); // public key
+	push_string(NULL, 0);
+
+	// public key
+
+	push_string(p->public_key, 64);
 
 	list(5);
 	msgdata = pop();
