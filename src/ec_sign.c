e0ebcfc406ad241f6ca0125348da7e5a548db48f
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-01-25 16:35:48 -0700

    Update ec_sign.c

--- src/ec_sign.c
+++ src/ec_sign.c
@@ -129,7 +129,7 @@ ec_sign(uint8_t *sig, uint8_t *hash, uint8_t *private_key)
 
 			ec_mod(s, q256);
 
-			if (ec_equal(s, 0) || ec_cmp(s, q256half) > -1) {
+			if (ec_equal(s, 0)) {
 				ec_free(k);
 				ec_free(r);
 				ec_free(s);
